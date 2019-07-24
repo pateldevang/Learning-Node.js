e1b6cf8603dd5b4683258dce04b888bd004d95c9
pateldevang <devangpatel92@gmail.com>
2019-07-25 00:12:12 +0530

    note add function

--- Note-app/app.js
+++ Note-app/app.js
@@ -7,6 +7,12 @@ const notes = require('./notes.js');
 
 var user = os.userInfo()
 var userName = os.userInfo().username
+var res = notes.addNote();
+console.log(res);
+
+var add =notes.add(12,-12)
+console.log(add);
+
 
 console.log(user);
 console.log(userName);
--- Note-app/notes.js
+++ Note-app/notes.js
@@ -1,3 +1,12 @@
 console.log('Starting Note.js');
 
-module.exports.age=25
+module.exports.addNote = () => {
+
+console.log('addNotes');
+return 'New Notes'
+};
+
+
+module.exports.add = (a,b) => {
+  return a+b
+};
