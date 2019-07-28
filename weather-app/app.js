75675037dd11271831d5e4a1d91cc159dca47ffa
pateldevang <devangpatel92@gmail.com>
2019-07-28 19:20:03 +0530

    utils and callback for geocode added

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 // const apiKey = '0f80134fe8fcc07592bece6452314334'
 // const baseURL = 'https://api.darksky.net'
@@ -19,16 +20,30 @@ const request = require('request')
 
 // Geocoding
 
-const geocodeurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Kolhapur.json?access_token=pk.eyJ1IjoiZGV2YW5ncGF0ZWwiLCJhIjoiY2p5bHNjd3p5MGJwYzNpcXI5bDA0bnhxOCJ9.Sf-JnXE2_TAU6ohmMvxphA'
-request({url : geocodeurl, json: true}, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to location service.')
-  } else if (response.body.features === undefined) {
-        console.log('Unable to find location. Try another search.');
-  } else {
-      const latitude = response.body.features[0].center[1]
-      const longitude = response.body.features[0].center[0]
-      console.log(latitude,longitude)
-  }
+// const geocodeurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Kolhapur.json?access_token=pk.eyJ1IjoiZGV2YW5ncGF0ZWwiLCJhIjoiY2p5bHNjd3p5MGJwYzNpcXI5bDA0bnhxOCJ9.Sf-JnXE2_TAU6ohmMvxphA'
+// request({url : geocodeurl, json: true}, (error, response) => {
+//   if (error) {
+//     console.log('Unable to connect to location service.')
+//   } else if (response.body.features === undefined) {
+//         console.log('Unable to find location. Try another search.');
+//   } else {
+//       const latitude = response.body.features[0].center[1]
+//       const longitude = response.body.features[0].center[0]
+//       console.log(latitude,longitude)
+//   }
+//
+// })
 
+
+
+
+
+
+
+
+
+
+geocode('VIT University', (error,data) => {
+  console.log('Error',error);
+  console.log('Data',data);
 })
