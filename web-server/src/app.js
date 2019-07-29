f0a8f13aa63cdab4f95909df470f6a4a12da1f7f
pateldevang <devangpatel92@gmail.com>
2019-07-29 22:21:28 +0530

    html and css added.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,27 +1,39 @@
 const express = require('express')
+const path = require('path')
+
+
+
+
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+
+app.use(express.static(publicDirectoryPath))
 
 
 
 
 
-app.get('',(req, res) => {
-    res.send('<h1>Weather</h1>')
-})
+// app.get('',(req, res) => {
+//     res.send('<h1>Weather</h1>')
+// })
 
 
-app.get('/help',(req, res) => {
-    res.send({
-        title: 'test',
-        num: 123
-    })
-})
+// app.get('/help',(req, res) => {
+//     res.send({
+//         title: 'test',
+//         num: 123
+//     })
+// })
 
 
-app.get('/about',(req, res) => {
-    res.send('<h1>About page</h1>')
-})
+// app.get('/about',(req, res) => {
+//     res.send('<h1>About page</h1>')
+// })
 
 app.get('/weather',(req, res) => {
     res.send({
