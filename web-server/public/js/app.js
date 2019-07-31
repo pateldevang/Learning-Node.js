cb6bb007211d9c49bc2220d332ddfcfe17065630
pateldevang <devangpatel92@gmail.com>
2019-07-31 23:00:39 +0530

    event listener added

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -10,3 +10,12 @@ fetch('http://localhost:3000/weather?address=Mumbai').then((response) => {
         }
     })
 })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log('Testing!')
+})
