513b1ec232ac674c9dac3eb5c1836e1527e012f8
pateldevang <devangpatel92@gmail.com>
2019-07-31 23:30:37 +0530

    index page message after fetch function added

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,19 +2,24 @@ console.log('Client side java script file loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
 
+    messageOne.textContent = 'Loading....'
+    messageTwo.textContent = ''
+
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
-            console.log(data.error)
+            messageOne.textContent = data.error
         } else {
-            console.log(data.location)
-            console.log(data)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast.summary + ' It is currently ' + data.forecast.temperature + ' degress out.' + ' There is a ' + data.forecast.rainProbability + '% chance of rain.'
         }
     })
 })
