9e628d9575290b97a3c03099699eedc1afdaa7b0
pateldevang <devangpatel92@gmail.com>
2019-07-31 21:55:01 +0530

    weather end points added

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -46,10 +48,25 @@ app.get('/weather', (req, res) => {
             error: 'Proper address required'
         })
     }
-    
-    res.send({
-        forecast: 'It is hot',
-        location: req.query.address
+
+
+    geocode(req.query.address, (error, { latitude, longitude, location}) => {
+
+        if (error) {
+            return res.send({error})
+        }
+        forecast(latitude,longitude, (error, forecastData) => {
+            if (error) {
+                res.send({error})
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+
+        })
     })
 })
 
