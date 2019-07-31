fe99113bf136de2200988e0894fca62cc65aec95
pateldevang <devangpatel92@gmail.com>
2019-07-31 20:54:02 +0530

    hbs footer and header added

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,81 +1,67 @@
-const express = require('express')
 const path = require('path')
-
+const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-
-// Defines paths for Express config
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname,'../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
-app.set('view engine','hbs')
-app.set('views',viewsPath)
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather app',
-        name: 'Andrew'
+        title: 'Weather',
+        name: 'Devang Patel'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew'
+        name: 'Devang Patel'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Help Page',
-        name: 'Andrew'
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Devang Patel'
     })
 })
 
-
-
-
-// app.get('',(req, res) => {
-//     res.send('<h1>Weather</h1>')
-// })
-
-
-// app.get('/help',(req, res) => {
-//     res.send({
-//         title: 'test',
-//         num: 123
-//     })
-// })
-
-
-// app.get('/about',(req, res) => {
-//     res.send('<h1>About page</h1>')
-// })
-
-app.get('/weather',(req, res) => {
+app.get('/weather', (req, res) => {
     res.send({
-        temperature: 23,
-        location: 'Hosur'
+        forecast: 'It is hot',
+        location: 'Vellore'
     })
 })
 
-app.get('/help/*',(req, res) => {
-    res.send('Help article not found')
-
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
 })
 
-app.get('*',(req, res) => {
-    res.send('My 404 page.')
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Devang Patel',
+        errorMessage: 'Page not found.'
+    })
 })
-
-
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.')    
+    console.log('Server is up on port 3000.')
 })
