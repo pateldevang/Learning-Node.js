d0c797b081dc8f5bacd4d72edd5e707579fec2e5
pateldevang <devangpatel92@gmail.com>
2019-08-02 23:32:02 +0530

    connection url and databse added

--- task-manager/mongoDB.js
+++ task-manager/mongoDB.js
@@ -3,5 +3,15 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
-const connectioURL = ''
-const database = ''
+const connectioURL = 'mongodb://127.0.0.1:27017'
+const database = 'task-manager'
+
+MongoClient.connect(connectioURL, { useNewUrlParser: true }, (error, client) => {
+
+    if (error) {
+        return console.log('Unable to connect to the database!')
+    }
+
+    console.log('Connected correctly!')
+
+})
