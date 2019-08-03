164e60a5939c33654776d44632069db9e6e0a42f
pateldevang <devangpatel92@gmail.com>
2019-08-03 14:38:52 +0530

    reject and resolve

--- Playground/3-callbacks.js
+++ Playground/3-callbacks.js
@@ -1,38 +1,14 @@
-// setTimeout(() => {
-//   console.log('Two second are up!');
-// }, 2000)
-//
-//
-// const names = ['Andrew','Johnny','Jess']
-// const shortName = names.filter((names) => {
-//   return names.length <= 4
-// })
-//
-//
-// const geocode = (address,callback) => {
-//   setTimeout(() => {
-//     const data = {
-//       latitude: '0',
-//       longitude: '0'
-//     }
-//       callback(data)
-//   }, 2000)
-// }
-//
-// geocode('Vellore', (data1) => {
-//   console.log(data1);
-// })
-
-
-const add = (a,b,callback) => {
+const doWorkCallback = (callback) => {
   setTimeout(() => {
-    c = a + b
-    callback(c)
+    // callback('This is my error', undefined)
+    callback(undefined, [1,4,7])
   }, 2000)
 }
 
 
-
-add(1,4, (sum) => {
-  console.log(sum);
+doWorkCallback((error, result) => {
+  if (error) {
+    return console.log(error)
+  }
+  console.log(result)
 })
--- Playground/7-promises.js
+++ Playground/7-promises.js
@@ -1,1 +1,13 @@
+const doWorkPromise = new Promise((resolve ,reject) => {
+    setTimeout(() => {
+        // resolve([7,4,1])
+        reject('Things went wrong!')
+    }, 2000)
+})
 
+
+doWorkPromise.then((result) => {
+    console.log('Sucess!', result)
+}).catch((error) => {
+    console.log('Error!',error)
+})
