a95387aca98f6ac3f45e8cb4ef660667ab330c2c
pateldevang <devangpatel92@gmail.com>
2019-08-03 22:22:50 +0530

    promises learn

--- Playground/7-promises.js
+++ Playground/7-promises.js
@@ -1,6 +1,6 @@
 const doWorkPromise = new Promise((resolve ,reject) => {
     setTimeout(() => {
-        // resolve([7,4,1])
+        // resolve([7,4,1]) 
         reject('Things went wrong!')
     }, 2000)
 })
