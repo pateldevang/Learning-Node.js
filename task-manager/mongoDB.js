8b9e06e89865fd7f09d53754747584e06b5a85bd
pateldevang <devangpatel92@gmail.com>
2019-08-03 12:34:16 +0530

    extra code removed

--- task-manager/mongoDB.js
+++ task-manager/mongoDB.js
@@ -21,52 +21,6 @@ MongoClient.connect(connectioURL, { useNewUrlParser: true }, (error, client) => 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'vikram',
-    //     age: 26
-    // }, (error, result) => {
-    //     if (error) {
-    //         console.log('Unable to insert user')
-    //     }
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Jen',
-    //         age: 28
-    //     }, {
-    //         name: 'Andrew',
-    //         age: 27
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log(error)
-    //     }
-    //     console.log(result.ops)
-    // })
-
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Buy milk & eggs',
-    //         completed: false
-    //     }, {
-    //         description: 'Prepare egg',
-    //         completed: false
-    //     }, {
-    //         description: 'Eat food',
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to add the job.')
-    //     }
-    //     console.log(result.ops)
-    // })
-
-
-
 
 
 })
